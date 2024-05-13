6389406b1c20510d063c7db6b71f1e69d1f34538
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-05-13 17:03:27 +0530

    checking dark mode in whole website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Voice Translation Transcription",
-  description: "Translates and Transcripts the voice",
+    title: "Voice Translation Transcription",
+    description: "Translates and Transcripts the voice",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} `}>{children}</body>
-    </html>
-  );
+        children: React.ReactNode;
+    }>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} `}>{children}</body>
+        </html>
+    );
 }
