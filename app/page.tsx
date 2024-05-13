aaf062095f4641e1c984c9332d6773e06e13ebf1
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-05-13 14:08:05 +0530

    adding transcribibg and information pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@
 import FileDisplay from "@/components/fileDisplay";
 import Header from "@/components/header";
 import Homepage from "@/components/homepage";
+import Information from "@/components/information";
+import Transcribing from "@/components/transcribing";
 import React, { useEffect } from "react";
 
 export default function Home() {
 
     const [file, setFile] = React.useState<File | null>(null);
     const [audioStream, setAudioStream] = React.useState<Blob | null>(null);
+    const [output, setOutput] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
 
     const isAudioAvailable = file || audioStream;
 
@@ -24,11 +28,15 @@ export default function Home() {
         <div className="flex flex-col max-w-[1000px] mx-auto w-full ">
             <section className="min-h-screen flex flex-col">
                 <Header />
-                { isAudioAvailable ? (
-                    <FileDisplay file={file} audioStream={audioStream} handleAudioReset={handleAudioReset} />
-                ) : (
-                        <Homepage setFile={setFile} setAudioStream={setAudioStream} />
-                    )}
+                { output ? (
+                    <Information />
+                ): loading? (
+                        <Transcribing />
+                    ): isAudioAvailable? (
+                            <FileDisplay file={file} audioStream={audioStream} handleAudioReset={handleAudioReset} />
+                        ): (
+                                <Homepage setFile={setFile} setAudioStream={setAudioStream} />
+                            ) }
             </section>
             <footer></footer>
         </div>
---
+++ components/information.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Information = () => {
+  return (
+    <div>information</div>
+  )
+}
+
+export default Information
